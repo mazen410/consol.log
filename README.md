7b6832bd64d7328af5e1a299e512296ab846d1e6
ᴅɢ:ᴇʟꜱᴏʀʏ (ꜱ) x-ᴅʀᴀᴠᴇ <mohammedmarei201178@gmail.com>
2026-08-18 18:37:18 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,71 @@
-# consol.log
+#README.md
+
+```markdown
+# @ASSG0/log
+
+A beautiful colored console logger with fancy ASCII banner for Node.js.
+
+---
+
+## Features
+- Fancy ASCII art banner on startup
+- Colored logging with timestamps
+- Multiple log types: info, success, error, warn, command
+- Lightweight and easy to use
+
+## Installation
+
+```bash
+npm install @ASSG0/log
+```
+
+## Usage
+
+```js
+const { log } = require('@ASSG0/log');
+
+log("This is a normal message");
+log.info("This is an info message");
+log.success("Operation completed successfully!");
+log.error("Something went wrong!");
+log.warn("This is a warning");
+log.command("Running command...");
+
+log.success("User logged in", "Rahul");
+```
+
+---
+
+## ⚠️ COPYRIGHT NOTICE
+
+/*
+ * © 2026 SHIX (X-DRAVE)
+ *
+ * ❌ DO NOT CLAIM THIS CODE AS YOUR OWN
+ *
+ * ✔️ Usage or modification is allowed ONLY
+ * with prior permission and proper credit.
+ *
+ * OFFICIAL LINKS (ONLY):
+ * YouTube   : https://youtube.com/@Z3N0SP
+ * TikTok : https://www.tiktok.com/@z3n0sp
+ * Telegram  : https://t.me/ASSG0
+ * GitHub    : https://github.com/mazen410
+ * WhatsApp  : +201034685714
+ *
+ */
+
+**Any form of re-uploading, recoding, modification, selling, or redistribution WITHOUT explicit permission from the original author is strictly prohibited.**
+
+**❌ NO CREDIT = NO PERMISSION**
+
+This package contains protection. If the copyright header is removed or modified in the source code, the package will automatically call `process.exit(1)` and crash.
+
+Do not remove the header if you want the code to run.
+
+**Violations may result in DMCA takedown.**
+
+---
+
+**© 2026 SHIX (X-DRAVE) | All Rights Reserved**
+```
---
+++ index.js
@@ -0,0 +1,125 @@
+//README.md
+
+/*
+ * © 2026 SHIX (X-DRAVE)
+ *
+ * ❌ DO NOT CLAIM THIS CODE AS YOUR OWN
+ *
+ * ✔️ Usage or modification is allowed ONLY
+ * with prior permission and proper credit.
+ *
+ * OFFICIAL LINKS (ONLY):
+ * YouTube   : https://youtube.com/@Z3N0SP
+ * TikTok : https://www.tiktok.com/@z3n0sp
+ * Telegram  : https://t.me/ASSG0
+ * GitHub    : https://github.com/mazen410
+ * WhatsApp  : +201034685714
+ *
+ */
+ 
+ console.clear();
+ 
+ console.log(`\x1b[38;5;195m-\x1b[38;5;194m-\x1b[38;5;189m-\x1b[38;5;159m-\x1b[38;5;153m-\x1b[38;5;147m-\x1b[38;5;195m-\x1b[38;5;194m-\x1b[38;5;189m-\x1b[38;5;159m SHIX \x1b[38;5;153m-\x1b[38;5;147m-\x1b[38;5;195m-\x1b[38;5;194m-\x1b[38;5;189m-\x1b[38;5;159m-\x1b[38;5;153m-\x1b[0m
+\x1b[38;5;195m⠀⠀⠀⠀⠀⠀⠀⣠⣤⣴⣶⣦⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\x1b[0m
+\x1b[38;5;194m⠀⠀⠀⢀⣠⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\x1b[0m
+\x1b[38;5;189m⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\x1b[0m
+\x1b[38;5;159m⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\x1b[0m
+\x1b[38;5;153m⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\x1b[0m
+\x1b[38;5;147m⠉⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\x1b[0m
+\x1b[38;5;195m⠀⢿⣿⣿⣿⣿⣿⡿⠛⢿⣿⣿⢿⢿⣿⣿⣽⣿⣿⣷⣶⣶⣶⣤⣄⠀⠀⠀⠀⠀⠀⠀⠀\x1b[0m
+\x1b[38;5;194m⠀⠀⠻⣿⣿⣿⣿⣷⡐⠻⠿⠁⠀⠘⠛⡟⠛⠛⢿⣿⣿⣿⡿⣿⣿⣿⣦⠀⠀⠀⠀⠀⠀\x1b[0m
+\x1b[38;5;189m⠀⠀⠀⠙⠿⢿⣿⣿⣿⣷⡄⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠀⠀⠀⠀\x1b[0m
+\x1b[38;5;159m⠀⠀⠀⠀⠀⠈⠙⢻⣿⠉⠛⢶⣄⣀⡀⠀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀\x1b[0m
+\x1b[38;5;153m⠀⠀⠀⠀⠀⠀⢠⡿⣧⣀⣲⢾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠀⠀⠀\x1b[0m
+\x1b[38;5;147m⠀⠀⠀⠀⠀⢰⡟⠀⠀⠈⠙⠟⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀\x1b[0m
+\x1b[38;5;195m⠀⠀⠀⠀⢠⡟⠁⠀⠀⠀⠀⠀⣾⣿⣿⠛⠻⠟⠛⣻⣿⣿⣿⣿⣿⠈⣿⣿⣿⣧⠀⠀⠀\x1b[0m
+\x1b[38;5;194m⠀⠀⠀⠀⣾⡇⠀\x1b[38;5;159mS\x1b[38;5;153m\x1b[38;5;147m7\x1b[0m⠀⠀⠀⠈⠻⣿⣾⡿⠀⠈⠉⠛⠉⠈⣿⣿⣾⣿⣿⣿⣿⠀⠀⠀\x1b[0m
+\x1b[38;5;189m⠀⠀⠀⠀⣿⠇⠀⣤⡀⠀⠀⠀⠀⠀⠘⣷⣀⠀⠀⠀⠀⠀⣠⣿⣿⣿\x1b[38;5;159mS\x1b[0m\x1b[38;5;153mY\x1b[0m⣿⣿⡆⠀⠀\x1b[0m
+\x1b[38;5;159m⠀⠀⠀⠀⣿⠀⠀⢻⣷⠀⠀⠀⠀⠀⠀⠈⠻⣶⣤⣴⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀\x1b[0m
+\x1b[38;5;153m⠀⠀⠀⠀⣿⠀⠀⠘⢿⣷⠢⠀⠀⠀⠀⠀⠀⣽⣿⡿⢛⠁⣠⠘⢻⣿⣿⣿⣿⣿⣿⣿⡀\x1b[0m
+\x1b[38;5;147m⠀⠀⠀⢀⣿⠀⠀⠀⠀⣙⣷⡀⠀⠀⣀⣤⡿⠋⠙⢿⣦⡀⠁⠀⠰⣿⣿⣿⣿⣿⣿⣿⣷\x1b[0m
+\x1b[38;5;195m⠀⠀⢰⣿⣿⣷⠾⠿⠛⢿⣿⣷⣴⣾⣿⣯⡀⠀⠀⠀⠉⠛⠿⢶⣴⣿⣿⣿⣿⣿⣿⣿⠟\x1b[0m
+\x1b[38;5;194m⠀⠀⣾⣿⣿⣉⠰⠀⠒⠀⠹⢿⣿⣿⣿⣿⣿⣶⣤⣀⡀⠀⠀⠀⠉⠉⠙⣿⣿⣿⣿⣿⡆\x1b[0m
+\x1b[38;5;189m⠀⠀⠉⢿⣿⣿⣿⣶⣶⣶⣶⣾⣿⡿⠿⠟⢻⣿⣿⣿⣿⣿⣶⣶⣶⣶⣴⣿⣿⣿⣿⡿⠀\x1b[0m
+\x1b[38;5;159m⠀⠀⠀⠀⢸⡇⢠⡀⠀⠀⠀⠀⠀⠀⠀⠀⢺⣿⠿⠛⢿⣟⠛⠛⠿⣿⡟⠋⠋⠉⠉⠀⠀\x1b[0m
+\x1b[38;5;153m⠀⠀⠀⠀⣼⠁⠘⢷⡀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠐⠀⠘⣿⣦⠀⠀⢹⣧⠀⠀⠀⠀⠀⠀\x1b[0m
+\x1b[38;5;147m⠀⠀⠀⠀⣿⠀⠀⠈⢿⡀⠀⠀⠀⠀⠀⠀⠘⣿⡆⠸⡇⠘⣿⡄⠀⢸⣿⡆⠀⠀⠀⠀⠀\x1b[0m
+\x1b[38;5;195m⠀⠀⠀⢰⡟⠀⠀⠀⠀⠻⡄⠀⠀⠀⠀⠀⠀⣿⡇⠀⠁⠀⠍⠇⠀⠈⣿⣧⠀⠀⠀⠀⠀\x1b[0m
+\x1b[38;5;194m⠀⠀⠀⠀⣧⣀⡀⠀⠀⠀⠉⠀⠀⠀⠀⢀⣠⣿⣿⠸⠀⠈⠀⠑⢀⠀⢹⣿⡆⠀⠀⠀⠀\x1b[0m
+\x1b[38;5;189m⠀⠀⠀⠀⢸⡿⣿⣿⣿⣷⣾⣿⡿⠿⠛⠿⣿⣿⣿⡀⠀⢆⠄⡤⣠⢠⠘⣿⣇⠀⠀⠀⠀\x1b[0m
+\x1b[38;5;159m⠀⠀⠀⠀⢸⣷⣿⣷⣿⡀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣇⠀⠈⠁⠁⠉⣠⣴⣿⣿⠀⠀⠀⠀\x1b[0m
+\x1b[38;5;147m⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠋⠉⠀⠀\x1b[0m\n\x1b[38;5;195m------------------------------------------\x1b[0m`);
+
+const fs=require('fs');const _0x592a=['from','utf8','base64','toString','readFileSync','ICogwqkgMjAyNiBTZVh5eGVvbiAoVk9JRFNFQykKICoKICog4pqg77iPIENPUFlSSUdIVCBOT1RJQ0UKICogVGhpcyBzb3VyY2UgY29kZSBpcyBwcm90ZWN0ZWQgdW5kZXIgY29weXJpZ2h0IGxhdy4KICogQW55IGZvcm0gb2YgcmUtdXBsb2FkaW5nLCByZWNvZGluZywgbW9kaWZpY2F0aW9uLAogKiBzZWxsaW5nLCBvciByZWRpc3RyaWJ1dGlvbiBXSVRIT1VUIGV4cGxpY2l0IHBlcm1pc3Npb24KICogZnJvbSB0aGUgb3JpZ2luYWwgYXV0aG9yIGlzIHN0cmljdGx5IHByb2hpYml0ZWQu','CiAqCiAqIOKdjCBOTyBDUkVESVQgPSBOTyBQRVJNSVNTSU9OCiAqIOKdjCBETyBOT1QgQ0xBSU0gVEhJUyBDT0RFIEFTIFlPVVIgT1dOCiAq','CiAqCiAqIOKclO+4jyBVc2FnZSBvciBtb2RpZmljYXRpb24gaXMgYWxsb3dlZCBPTkxZCiAqIHdpdGggcHJpb3IgcGVybWlzc2lvbiBhbmQgcHJvcGVyIGNyZWRpdC4KICoKICogT0ZGSUNJQUwgTElOS1MgKE9OTFkpOgogKiBZb3VUdWJlICAgOiBodHRwczovL3lvdXR1YmUuY29tL0B2b2lkc2VjNzcxOAogKiBJbnN0YWdyYW0gOiBzYWJpci5fNzcxOAogKiBUZWxlZ3JhbSAgOiBodHRwczovL3QubWUvU0FCSVI3NzE4CiAqIEdpdEh1YiAgICA6IGh0dHBzOi8vZ2l0aHViLmNvbS9TQUJJUjc3MTgKICogV2hhdHNBcHAgIDogKzkxIDczNjUwIDg1MjEzCg=='];const _0x4f21=(i)=>_0x592a[i];(function(){try{const _0xpath=require.main?.filename;if(!_0xpath)return;const _0xraw=_0x4f21(5)+_0x4f21(6)+_0x4f21(7);const _0xkey=Buffer.from(_0xraw,'base64').toString('utf8').trim();const _0xcode=fs.readFileSync(_0xpath,'utf8');if(!_0xcode.includes(_0xkey.substring(0,50))){process.exit(1);}}catch(e){process.exit(1);}})();
+
+const colors = {
+  reset: "\x1b[0m",
+  black: "\x1b[30m",
+  red: "\x1b[31m",
+  green: "\x1b[32m",
+  yellow: "\x1b[33m",
+  blue: "\x1b[34m",
+  magenta: "\x1b[35m",
+  cyan: "\x1b[36m",
+  white: "\x1b[37m",
+  gray: "\x1b[90m",
+  brightRed: "\x1b[91m",
+  brightGreen: "\x1b[92m",
+  brightYellow: "\x1b[93m",
+  brightBlue: "\x1b[94m",
+  brightMagenta: "\x1b[95m",
+  brightCyan: "\x1b[96m",
+  brightWhite: "\x1b[97m"
+};
+
+function log(typeOrMessage, userOrColor, maybeMessage) {
+  const time = new Date().toLocaleTimeString();
+  const timestamp = `${colors.gray}[${time}]${colors.reset}`;
+  let type = "info";
+  let user = null;
+  let message = "";
+  if (maybeMessage !== undefined) {
+    type = typeOrMessage;
+    user = userOrColor;
+    message = maybeMessage;
+  }
+  else if (colors[userOrColor]) {
+    message = typeOrMessage;
+    type = userOrColor;
+  }
+  else {
+    message = typeOrMessage;
+  }
+  const colorMap = {
+    yellow: "warn",
+    red: "error",
+    green: "success",
+    cyan: "info",
+    blue: "info",
+    magenta: "command"
+  };
+
+  if (colorMap[type]) {
+    type = colorMap[type];
+  }
+
+  let typeTag = "";
+  if (type === "info") typeTag = `${colors.blue}INFO${colors.reset}`;
+  else if (type === "success") typeTag = `${colors.green}SUCCESS${colors.reset}`;
+  else if (type === "error") typeTag = `${colors.red}ERROR${colors.reset}`;
+  else if (type === "warn") typeTag = `${colors.yellow}WARN${colors.reset}`;
+  else if (type === "command") typeTag = `${colors.magenta}CMD${colors.reset}`;
+  else typeTag = `${colors.white}LOG${colors.reset}`;
+
+  const userTag = user
+    ? `${colors.cyan}${user}${colors.reset}`
+    : `${colors.yellow}SYSTEM${colors.reset}`;
+
+  console.log(`${timestamp} | ${typeTag} | ${userTag} | ${message}`);
+}
+
+["info", "success", "error", "warn", "command"].forEach(type => {
+  log[type] = (message, user = null) => log(type, user, message);
+});
+
+module.exports = { log };
